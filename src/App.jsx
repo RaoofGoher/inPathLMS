c497a4d78ca24dcac2a96b4e121d459fc3ae64ec
Raoof Goher <raoofgohersd@gmail.com>
2024-11-01 20:48:07 +0500

    initiail setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="bg-red-200 text-center">Hello, World!</h1>
+        <h1 className="bg-red-200 text-center font-lato text-8xl">Hello, World!</h1>
+        <div className="flex justify-center">
+          <div className="flex items-center bg-blue-200 p-10 text-center">
+            <h2 className="text-8xl font-montserrat">This is a responsive website</h2>
+          </div>
+        </div>
         </div>
     </>
   )
