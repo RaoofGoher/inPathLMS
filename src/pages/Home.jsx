392cc2e1c936b53cb604b1d8c12253058757edc5
Raoof Goher <raoofgohersd@gmail.com>
2024-11-06 03:51:13 +0500

    two more components added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import HowItWorks from '../components/HowITWorks'
+import JoinOurTeam from '../components/JoinUs'
 
 const Home = () => {
   return (
     <div>
       <HeroSection className = "mt-[-140px]"/>
       <HowItWorks />
+      <JoinOurTeam/>
     </div>
   )
 }
