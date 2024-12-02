6ffea3bd2c54db8873f98fe67002339ade4b7cbe
Raja Anas <buttans340@gmail.com>
2024-12-02 19:26:47 +0500

    conflict resolve

--- src/components/PrimaryNavbar.jsx
+++ src/components/PrimaryNavbar.jsx
@@ -1,24 +1,23 @@
 // Navbar.js
-import React, { useState } from 'react';
-import { NavLink, Link, useNavigate } from 'react-router-dom';
-import SearchBar from './SearchBar';
-import Logo from "../assets/logos/Logo.png"
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../features/auth/authSlice';
+import React, { useState } from "react";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import Logo from "../assets/logos/Logo.png";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../features/auth/authSlice";
 
 const Navbar = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const { token, role, isAuthenticated, user_id } = useSelector((state) => state.auth);
+  const { token, role, isAuthenticated, user_id } = useSelector(
+    (state) => state.auth
+  );
   const navigate = useNavigate(); // Use the navigate hook to redirect to other pages
   const dispatch = useDispatch();
 
-
-
   const handleLogout = () => {
-    dispatch(logout())
-    navigate('/login')
-
-  }
+    dispatch(logout());
+    navigate("/login");
+  };
 
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
@@ -27,21 +26,19 @@ const Navbar = () => {
   return (
     <nav className="my-4 shadow-md px-16 py-4 bg-primaryColor">
       <div className="container mx-auto flex items-center justify-between">
-
-        {/* Left Side: Logo */}
+        {/* {/ Left Side: Logo /} */}
         <div className="flex items-center flex-1">
-          <NavLink to={"/"}><img src={Logo} className='mr-16 w-[4rem]' alt="Logo" /></NavLink>
-          {/* Search Bar on Larger Screens Only */}
+          <NavLink to={"/"}>
+            <img src={Logo} className="mr-16 w-[4rem]" alt="Logo" />
+          </NavLink>
+          {/* {/ Search Bar on Larger Screens Only /} */}
           <SearchBar />
         </div>
 
-        {/* Right Side: Drawer Button and Login & Signup Buttons */}
+        {/* {/ Right Side: Drawer Button and Login & Signup Buttons /} */}
         <div className="flex items-center">
-          <button
-            className="md:hidden text-white"
-            onClick={toggleDrawer}
-          >
-            {/* Icon for toggling drawer (hamburger icon) */}
+          <button className="md:hidden text-white" onClick={toggleDrawer}>
+            {/* {/ Icon for toggling drawer (hamburger icon) /} */}
             <svg
               className="w-6 h-6"
               fill="none"
@@ -49,85 +46,91 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
             </svg>
           </button>
-          {isAuthenticated === true ? <>
-            {/* buttons after login */}
-            <div className="hidden md:flex space-x-4">
-
-              <NavLink
-
-                to={
-                  role === 'admin'
-                    ? '/admin-dashboard'
-                    : role === 'instructor'
-                      ? '/dashboard/teacherdashboard'
-                      : role === 'student'
-                        ? '/dashboard/studentdashboard'
-                        : '/login'
-                }
-
-
-
-                className={({ isActive }) =>
-                  isActive ? "bg-lightColor1 rounded-md" : ""
-                }
-                end>
-                <button className="px-4 py-2 border text-white border-secondaryColor rounded-md hover:text-secondaryColor transition text-secondaryColor">
-                  Dashboard
-                </button>
-              </NavLink>
-              <NavLink 
-              >
-                <button onClick={handleLogout} className="px-4 py-2 border border-secondaryColor hover:text-secondaryColor rounded-md text-white">
-                  Logout
-                </button>
-              </NavLink>
-            </div> </> :
-
-
+          {isAuthenticated === true ? (
             <>
-
-              {/* buttons before login */}
+              {/* {/ buttons after login /} */}
               <div className="hidden md:flex space-x-4">
-                <NavLink end to={'/teachersignup'} className={({ isActive }) =>
-                  isActive ? "bg-lightColor1 rounded-md" : ""
-                }>
+                <NavLink
+                  to={
+                    role === "admin"
+                      ? "/admin-dashboard"
+                      : role === "instructor"
+                      ? "/dashboard/teacherdashboard"
+                      : role === "student"
+                      ? "/dashboard/studentdashboard"
+                      : "/login"
+                  }
+                  className={({ isActive }) =>
+                    isActive ? "bg-lightColor1 rounded-md" : ""
+                  }
+                  end
+                >
+                  <button className="px-4 py-2 border text-white border-secondaryColor rounded-md hover:text-secondaryColor transition text-secondaryColor">
+                    Dashboard
+                  </button>
+                </NavLink>
+                <NavLink>
+                  <button
+                    onClick={handleLogout}
+                    className="px-4 py-2 border border-secondaryColor hover:text-secondaryColor rounded-md text-white"
+                  >
+                    Logout
+                  </button>
+                </NavLink>
+              </div>{" "}
+            </>
+          ) : (
+            <>
+              {/* {/ buttons before login /} */}
+              <div className="hidden md:flex space-x-4">
+                <NavLink
+                  end
+                  to={"/teachersignup"}
+                  className={({ isActive }) =>
+                    isActive ? "bg-lightColor1 rounded-md" : ""
+                  }
+                >
                   <button className="px-4 py-2 hover:text-secondaryColor rounded-md text-white">
                     Teach On In Path
                   </button>
                 </NavLink>
-                <NavLink end to={"/login"} className={({ isActive }) =>
-                  isActive ? "bg-lightColor1 rounded-md" : ""
-                }>
+                <NavLink
+                  end
+                  to={"/login"}
+                  className={({ isActive }) =>
+                    isActive ? "bg-lightColor1 rounded-md" : ""
+                  }
+                >
                   <button className="px-4 py-2 border text-white border-secondaryColor rounded-md hover:text-secondaryColor transition text-secondaryColor">
                     Login
                   </button>
                 </NavLink>
-                <NavLink end to={'/studentsignup'} className={({ isActive }) =>
-                  isActive ? "bg-lightColor1 rounded-md" : ""
-                }>
+                <NavLink
+                  end
+                  to={"/studentsignup"}
+                  className={({ isActive }) =>
+                    isActive ? "bg-lightColor1 rounded-md" : ""
+                  }
+                >
                   <button className="px-4 py-2 border border-secondaryColor hover:text-secondaryColor rounded-md text-white">
                     Sign Up
                   </button>
                 </NavLink>
               </div>
-
             </>
-
-
-
-          }
-
-
-
-
-
+          )}
         </div>
       </div>
 
-      {/* Drawer Menu */}
+      {/* {/ Drawer Menu /} */}
       {isDrawerOpen && (
         <div className="fixed inset-0 z-50 bg-black bg-opacity-50 md:hidden">
           <div className="absolute top-0 right-0 w-64 bg-primaryColor h-full shadow-lg">
@@ -138,7 +141,7 @@ const Navbar = () => {
               close
             </button>
             <div className="p-4">
-              {/* Search Bar in Drawer */}
+              {/* {/ Search Bar in Drawer /} */}
               <input
                 type="text"
                 placeholder="Search..."
@@ -146,20 +149,44 @@ const Navbar = () => {
               />
             </div>
             <div className="flex flex-col p-4 space-y-2">
-              <NavLink to="/teachersignup" onClick={toggleDrawer} className={({ isActive }) =>
-                isActive ? "bg-lightColor1" : "border-b-2 border-secondaryColor"
-              } >
-                <button className="w-full text-left px-4 py-2 text-white hover:text-secondaryColor">Teach On In Path</button>
+              <NavLink
+                to="/teachersignup"
+                onClick={toggleDrawer}
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-lightColor1"
+                    : "border-b-2 border-secondaryColor"
+                }
+              >
+                <button className="w-full text-left px-4 py-2 text-white hover:text-secondaryColor">
+                  Teach On In Path
+                </button>
               </NavLink>
-              <NavLink to="/login" onClick={toggleDrawer} className={({ isActive }) =>
-                isActive ? "bg-lightColor1" : "border-b-2 border-secondaryColor"
-              }>
-                <button className="w-full text-left px-4 py-2 text-white hover:text-secondaryColor">Login</button>
+              <NavLink
+                to="/login"
+                onClick={toggleDrawer}
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-lightColor1"
+                    : "border-b-2 border-secondaryColor"
+                }
+              >
+                <button className="w-full text-left px-4 py-2 text-white hover:text-secondaryColor">
+                  Login
+                </button>
               </NavLink>
-              <NavLink to="/studentsignup" onClick={toggleDrawer} className={({ isActive }) =>
-                isActive ? "bg-lightColor1" : "border-b-2 border-secondaryColor"
-              }>
-                <button className="w-full text-left px-4 py-2 text-white hover:text-secondaryColor">Sign Up</button>
+              <NavLink
+                to="/studentsignup"
+                onClick={toggleDrawer}
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-lightColor1"
+                    : "border-b-2 border-secondaryColor"
+                }
+              >
+                <button className="w-full text-left px-4 py-2 text-white hover:text-secondaryColor">
+                  Sign Up
+                </button>
               </NavLink>
             </div>
           </div>
--- src/pages/learn/Certification.jsx
+++ src/pages/learn/Certification.jsx
@@ -1,78 +1,184 @@
-import React from 'react'
-import ScrollToTop from '../../components/ScrollToTop';
+import React from "react";
+import ScrollToTop from "../../components/ScrollToTop";
 
 const Certification = () => {
+  
+  // Scroll to the "certifications" section when the button is clicked
+  const handleScrollToCertification = (e) => {
+    e.preventDefault(); // Prevent default link behavior to handle scrolling manually
+    const section = document.getElementById("certifications"); // Correct target section ID
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" }); // Smooth scroll to that section
+    }
+  };
+  const handleScrollToHeroSection = (e) => {
+    e.preventDefault(); // Prevent default link behavior to handle scrolling manually
+    const section = document.getElementById("hero-section"); // Correct target section ID
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" }); // Smooth scroll to that section
+    }
+  };
+
   return (
-    <div className="bg-gradient-to-r from-[#4A90E2] to-[#50E3C2] text-white py-16">
-      <ScrollToTop/>
+    <div>
+      <ScrollToTop />
       {/* Hero Section */}
-      <section className="text-center mb-16">
-        <h1 className="text-4xl font-bold mb-4">Earn Your Certification and Boost Your Career</h1>
-        <p className="text-lg mb-8">Unlock new opportunities and gain the skills you need to succeed in your professional journey with our industry-recognized certifications.</p>
-        <a href="#certifications" className="bg-lightColor1 py-3 px-8 rounded-full text-lg font-semibold shadow-lg transition-transform transform hover:bg-secondaryColor hover:scale-105 hover:shadow-2xl hover:animate-pulse focus:outline-none focus:ring-4 focus:ring-turquoise focus:ring-opacity-50">Explore Certifications</a>
+      <section id="hero-section" className="h-screen text-center bg-gradient-to-r from-primaryColor to-secondaryColor flex flex-col justify-center items-center p-8 gap-4">
+        <h1 className="text-4xl font-bold mb-4 text-lightColor2">
+          Earn Your Certification and Boost Your Career
+        </h1>
+        <p className="text-lg mb-8 text-lightColor2">
+          Unlock new opportunities and gain the skills you need to succeed in
+          your professional journey with our industry-recognized certifications.
+        </p>
+        <div className="flex flex-col items-center gap-4 mb-8">
+          <p className="text-xl text-lightColor2">
+            Join thousands of professionals who have advanced their careers with
+            our expert-led training programs.
+          </p>
+          <div className="flex gap-4">
+            <span className="bg-lightColor2 px-4 py-2 rounded-md text-darkColor1 font-semibold">
+              500+ Courses
+            </span>
+            <span className="bg-lightColor2 px-4 py-2 rounded-md text-darkColor1 font-semibold">
+              10,000+ Graduates
+            </span>
+          </div>
+        </div>
+        <a
+          href="#certifications" // Use anchor link targeting #certifications
+          onClick={handleScrollToCertification} // Call scroll logic on click
+          className="bg-lightColor1 py-3 px-8 rounded-full text-lg font-semibold shadow-lg transition-transform transform hover:bg-secondaryColor hover:scale-105 hover:shadow-2xl hover:animate-pulse focus:outline-none focus:ring-4 focus:ring-turquoise focus:ring-opacity-50"
+        >
+          Explore Certifications
+        </a>
       </section>
 
       {/* Certification Benefits Section */}
-      <section className="bg-dark1 py-16">
+      <section className="bg-[#EAF4FA] py-16">
         <div className="max-w-7xl mx-auto px-6 text-center">
-          <h2 className="text-3xl font-semibold mb-8">Why Get Certified?</h2>
+          <h2 className="text-3xl font-semibold mb-8 text-primaryColor">
+            Why Get Certified?
+          </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="bg-gradient-to-bl from-secondaryColor to-primaryColor text-gray-800 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold mb-4">Career Advancement</h3>
-              <p className='text-white'>Certification can open doors to new job opportunities and higher salary potential.</p>
+            <div className="bg-gradient-to-bl from-[#50E3C2] to-[#4A90E2] text-white p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold mb-4">
+                Career Advancement
+              </h3>
+              <p className="text-white">
+                Certification can open doors to new job opportunities and higher
+                salary potential.
+              </p>
             </div>
-            <div className="bg-gradient-to-bl from-secondaryColor to-primaryColor text-gray-800 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold mb-4">Industry Recognition</h3>
-              <p className='text-white'>Gain credibility in your field and stand out to employers and peers.</p>
+            <div className="bg-gradient-to-bl from-[#50E3C2] to-[#4A90E2] text-white p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold mb-4">
+                Industry Recognition
+              </h3>
+              <p className="text-white">
+                Gain credibility in your field and stand out to employers and
+                peers.
+              </p>
             </div>
-            <div className="bg-gradient-to-bl from-secondaryColor to-primaryColor text-gray-800 p-6 rounded-lg shadow-lg">
+            <div className="bg-gradient-to-bl from-[#50E3C2] to-[#4A90E2] text-white p-6 rounded-lg shadow-lg">
               <h3 className="text-2xl font-semibold mb-4">Skill Mastery</h3>
-              <p className='text-white'>Demonstrate your mastery of key skills with proven expertise through certifications.</p>
+              <p className="text-white">
+                Demonstrate your mastery of key skills with proven expertise
+                through certifications.
+              </p>
             </div>
           </div>
         </div>
       </section>
 
       {/* List of Available Certifications */}
-      <section id="certifications" className="py-16">
+      <section id="certifications" className="py-16 bg-[#EAF4FA]">
         <div className="max-w-7xl mx-auto px-6 text-center">
-          <h2 className="text-3xl font-semibold mb-8">Our Popular Certifications</h2>
+          <h2 className="text-3xl font-semibold mb-8 text-primaryColor">
+            Our Popular Certifications
+          </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="bg-dark1 p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
-              <h3 className="text-2xl font-semibold text-white mb-4">Certified Web Developer</h3>
-              <p className="text-white mb-4">Complete our comprehensive course and gain certification as a web developer.</p>
-              <a href="#" className="text-[#4A90E2] font-semibold hover:underline">Learn More</a>
+              <h3 className="text-2xl font-semibold text-white mb-4">
+                Certified Web Developer
+              </h3>
+              <p className="text-white mb-4">
+                Complete our comprehensive course and gain certification as a
+                web developer.
+              </p>
+              <a
+                href="#"
+                className="text-[#4A90E2] font-semibold hover:underline"
+              >
+                Learn More
+              </a>
             </div>
             <div className="bg-dark1 p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
-              <h3 className="text-2xl font-semibold text-white mb-4">Data Science Certification</h3>
-              <p className="text-white mb-4">Master data analysis, visualization, and machine learning concepts with this certification.</p>
-              <a href="#" className="text-[#4A90E2] font-semibold hover:underline">Learn More</a>
+              <h3 className="text-2xl font-semibold text-white mb-4">
+                Data Science Certification
+              </h3>
+              <p className="text-white mb-4">
+                Master data analysis, visualization, and machine learning
+                concepts with this certification.
+              </p>
+              <a
+                href="#"
+                className="text-[#4A90E2] font-semibold hover:underline"
+              >
+                Learn More
+              </a>
             </div>
             <div className="bg-dark1 p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
-              <h3 className="text-2xl font-semibold text-white mb-4">Digital Marketing Expert</h3>
-              <p className="text-white mb-4">Get certified in the latest digital marketing techniques and tools.</p>
-              <a href="#" className="text-[#4A90E2] font-semibold hover:underline">Learn More</a>
+              <h3 className="text-2xl font-semibold text-white mb-4">
+                Digital Marketing Expert
+              </h3>
+              <p className="text-white mb-4">
+                Get certified in the latest digital marketing techniques and
+                tools.
+              </p>
+              <a
+                href="#"
+                className="text-[#4A90E2] font-semibold hover:underline"
+              >
+                Learn More
+              </a>
             </div>
           </div>
         </div>
       </section>
 
       {/* How It Works Section */}
-      <section className="bg-dark1 py-16">
+      <section className="bg-primaryColor py-16">
         <div className="max-w-7xl mx-auto px-6 text-center">
-          <h2 className="text-3xl font-semibold mb-8">How It Works</h2>
+          <h2 className="text-3xl font-semibold mb-8 text-darkColor1">
+            How It Works
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="bg-gradient-to-bl from-secondaryColor to-primaryColor p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-gray-800 mb-4">Step 1: Choose Your Certification</h3>
-              <p>Browse our list of certifications and choose the one that best fits your career goals.</p>
+            <div className="bg-gradient-to-bl from-[#50E3C2] to-[#4A90E2] p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold text-white mb-4">
+                Step 1: Choose Your Certification
+              </h3>
+              <p>
+                Browse our list of certifications and choose the one that best
+                fits your career goals.
+              </p>
             </div>
-            <div className="bg-gradient-to-bl from-secondaryColor to-primaryColor p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-gray-800 mb-4">Step 2: Complete the Course</h3>
-              <p>Follow our online courses, which include videos, assignments, and quizzes to help you master the skills.</p>
+            <div className="bg-gradient-to-bl from-[#50E3C2] to-[#4A90E2] p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold text-white mb-4">
+                Step 2: Complete the Course
+              </h3>
+              <p>
+                Follow our online courses, which include videos, assignments,
+                and quizzes to help you master the skills.
+              </p>
             </div>
-            <div className="bg-gradient-to-bl from-secondaryColor to-primaryColor p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-gray-800 mb-4">Step 3: Take the Exam</h3>
-              <p>After completing the course, take the exam to demonstrate your knowledge and skills.</p>
+            <div className="bg-gradient-to-bl from-[#50E3C2] to-[#4A90E2] p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold text-white mb-4">
+                Step 3: Take the Exam
+              </h3>
+              <p>
+                After completing the course, take the exam to demonstrate your
+                knowledge and skills.
+              </p>
             </div>
           </div>
         </div>
@@ -81,11 +187,20 @@ const Certification = () => {
       {/* Call to Action Section */}
       <section className="text-center py-16 bg-[#4A90E2] text-white">
         <h2 className="text-3xl font-semibold mb-4">Ready to Get Certified?</h2>
-        <p className="text-lg mb-8">Start your certification journey today and take the first step toward advancing your career.</p>
-        <a href="#" className="bg-lightColor1 py-3 px-8 rounded-full text-lg font-semibold shadow-lg transition-transform transform hover:bg-secondaryColor hover:scale-105 hover:shadow-2xl hover:animate-pulse focus:outline-none focus:ring-4 focus:ring-turquoise focus:ring-opacity-50">Get Started</a>
+        <p className="text-lg mb-8">
+          Start your certification journey today and take the first step toward
+          advancing your career.
+        </p>
+        <a
+          href="#hero-section"
+          onClick={handleScrollToHeroSection}
+          className="bg-lightColor1 py-3 px-8 rounded-full text-lg font-semibold shadow-lg transition-transform transform hover:bg-secondaryColor hover:scale-105 hover:shadow-2xl hover:animate-pulse focus:outline-none focus:ring-4 focus:ring-turquoise focus:ring-opacity-50"
+        >
+          Get Started
+        </a>
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default Certification;
