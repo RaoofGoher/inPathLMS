6016ca457daefb856175a1ca6136681e0856c81b
Raoof goher <raoofgohersd@gmail.com>
2024-12-24 22:04:42 +0500

    work in progress

--- src/components/PrimaryNavbar.jsx
+++ src/components/PrimaryNavbar.jsx
@@ -10,12 +10,41 @@ import { clearCart } from "../features/cart/cartSlice";
 import Dropdown from "./explore/ExploreDropDown";
 import icon from "../assets/language-icon.svg";
 import { resetSubCategoryID } from "../features/searchCourse/ExploreSubCategoryID";
+import {FaBell} from "react-icons/fa";
 const Navbar = () => {
+  
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { token, role, isAuthenticated, user_id } = useSelector(
     (state) => state.auth
   );
+  const [notifications, setNotifications] = useState([
+    {
+      id: 1,
+      message: "New student request from Jane Doe.",
+      type: "student",
+      name: "Jane Doe",
+      avatar: "https://via.placeholder.com/40",
+    },
+    {
+      id: 2,
+      message: "Teacher Mark Johnson submitted new course materials.",
+      type: "teacher",
+      name: "Mark Johnson",
+      avatar: "https://via.placeholder.com/40",
+    },
+    {
+      id: 3,
+      message: "New student enrollment from Alex Smith.",
+      type: "student",
+      name: "Alex Smith",
+      avatar: "https://via.placeholder.com/40",
+    },
+  ]);
+  const [isDropdownOpen1, setIsDropdownOpen1] = useState(false);
+
+
+
   const navigate = useNavigate(); // Use the navigate hook to redirect to other pages
   const dispatch = useDispatch();
 
@@ -28,6 +57,16 @@ const Navbar = () => {
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
+
+  const handleNotificationClick = () => {
+    setIsDropdownOpen1(!isDropdownOpen1);
+  };
+
+  const handleNotificationItemClick = () => {
+    setIsDropdownOpen1(false); // Close dropdown when notification is clicked
+  };
+
+
 
   return (
     <nav className=" px-16 py-4 border-b-2 bg-white">
@@ -76,6 +115,51 @@ const Navbar = () => {
           </button>
           {isAuthenticated === true ? (
             <>
+
+              <div
+                className="relative flex justify-center items-center mr-4"
+                onClick={handleNotificationClick} // Open or close dropdown when clicked
+              >
+                <FaBell size={20} className="text-blueColor cursor-pointer" />
+                {/* Notification Badge */}
+                <span className="absolute -top-2 -right-2 flex justify-center items-center w-4 h-4 bg-red-600 text-white text-xs rounded-full">
+                  {notifications.length}
+                </span>
+              </div>
+
+              {/* Notifications Dropdown */}
+              {isDropdownOpen1 && (
+                <div className="z-50 absolute top-16 right-0 bg-white shadow-xl rounded-lg w-80 p-4 border border-grayColor">
+                  <h3 className="font-bold text-sm mb-2 text-blueColor">
+                    Recent Notifications
+                  </h3>
+                  <ul className="space-y-2 ">
+                    {notifications.map((notification) => (
+                      <li
+                        key={notification.id}
+                        className={`flex items-center space-x-2 text-sm p-4 rounded-lg bg-white hover:bg-gray-200 cursor-pointer transition-all duration-200 ${notification.type === "student"
+                            ? "text-blueColor"
+                            : "text-green-600"
+                          }`}
+                        onClick={handleNotificationItemClick} // Close dropdown on click
+                      >
+                        <img
+                          src={notification.avatar} // User's avatar image
+                          alt={notification.name}
+                          className="w-8 h-8 rounded-full"
+                        />
+                        <div>
+                          <p className="font-semibold">{notification.name}</p>{" "}
+                          {/* User Name */}
+                          <p>{notification.message}</p> {/* Notification Message */}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+
+
               {/* {/ buttons after login /} */}
               <div className="hidden md:flex space-x-4">
                 <NavLink
@@ -83,10 +167,10 @@ const Navbar = () => {
                     role === "admin"
                       ? "/dashboard/admin"
                       : role === "instructor"
-                      ? "/dashboard/teacherdashboard"
-                      : role === "student"
-                      ? "/dashboard/studentdashboard"
-                      : "/login"
+                        ? "/dashboard/teacherdashboard"
+                        : role === "student"
+                          ? "/dashboard/studentdashboard"
+                          : "/login"
                   }
                   className={({ isActive }) =>
                     isActive
