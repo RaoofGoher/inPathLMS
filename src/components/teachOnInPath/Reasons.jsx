e33923405895b6e1259e6346ca19efc242750ffa
Raoof goher <raoofgohersd@gmail.com>
2024-12-27 03:59:30 +0500

    work in progress

--- src/components/teachOnInPath/Reasons.jsx
+++ src/components/teachOnInPath/Reasons.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-
+import Benifits from '../../assets/teachOnInPath/benifits.png';
+import Inspire from '../../assets/teachOnInPath/Inspire.png';
+import Educate from '../../assets/teachOnInPath/educate.png';
 const Reasons = () => {
     const reasons = [
         {
             title: 'Educate your Style',
             description: 'Publish the course you want, in the way you want, and always have control of your own content.',
-            image: 'path/to/your/image1.jpg',
+            image: Educate,
         },
         {
             title: 'Inspire Students',
             description: 'Teach what you know and help learners explore their interests, gain new skills, and advance their careers.',
-            image: 'path/to/your/image2.jpg',
+            image: Inspire,
         },
         {
             title: 'Get benefits',
             description: 'Expand your professional network, build your expertise, and earn money on each paid enrollment.',
-            image: 'path/to/your/image3.jpg',
+            image: Benifits,
         },
     ];
 
     return (
         <div className="container mx-auto p-4">
-            <h2 className="text-3xl font-bold text-center mb-8">So many reasons to start</h2>
+            <h2 className="text-3xl font-bold text-center mb-8 font-montserrat text-blueColor">Reasons to Start</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
                 {reasons.map((reason, index) => (
                     <div key={index} className="overflow-hidden">
-                        <img src={reason.image} alt={reason.title} className="w-full h-48 object-cover" />
+                        <img src={reason.image} alt={reason.title} className="w-20 h-20 flex m-auto" />
                         <div className="p-4">
                             <h3 className="text-xl font-semibold mb-2">{reason.title}</h3>
                             <p className="text-gray-700">{reason.description}</p>
